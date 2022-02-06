d74506d797191b41c706c184dd6ec63a80d57a90
emmagomedov <emmagomedov@gmail.com>
2022-02-06 13:05:00 +0300

    Add User Panel

--- WorldSkillsApp/AddUser.xaml.cs
+++ WorldSkillsApp/AddUser.xaml.cs
@@ -31,25 +31,48 @@ namespace WorldSkillsApp
 
         private void AddUserOnData(object sender, RoutedEventArgs e)
         {
+            Dictionary<string, int> officesObj = new Dictionary<string, int> { };
+
             try
             {
                 string detectCountryIdCommand = "SELECT * FROM Offices";
                 var reader = sqlConnector.Queryes(detectCountryIdCommand);
 
-                Dictionary<string, int> officesObj = new Dictionary<string, int> { };
 
                 while (reader.Read())
                 {
                     officesObj[reader["Title"].ToString()] = Int32.Parse(reader["ID"].ToString());
                 }
-
-                foreach (var item in officesObj)
-                {
-                    MessageBox.Show(item[]);
-                }
             } catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                sqlConnector.sqlConnection.Close();
+            }
+
+            try
+            {
+                string showAllUsers = "SELECT max(ID) FROM Users";
+                var reader = sqlConnector.Queryes(showAllUsers);
+                int userId = 0;
+                PasswordBox password = new PasswordBox();
+
+                while (reader.Read()) userId = (int)reader[""];
+
+                string addUserCommand = $@"INSERT INTO Users VALUES ({userId++}, 2, '{email.Text}', {password.Password}, 
+                                            '{firstName.Text}', '{lastName.Text}', {officesObj[office.Text]}, '{birthDate.Text}', 1)";
+
+                sqlConnector.getQuery(addUserCommand);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                sqlConnector.sqlConnection.Close();
             }
         }
     }
